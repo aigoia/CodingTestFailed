82f0c4e266c4c65d7fc65a59d0c5a21649429ed0
aigoia <oidoicompany@gmail.com>
2023-01-13 18:54:39 +0900

    clean Audio

--- Assets/Coding/AudioManager.cs
+++ Assets/Coding/AudioManager.cs
@@ -5,10 +5,46 @@ namespace Game.Menu
     public class AudioManager : MonoBehaviour
     {
         public AudioSource source;
-
+        public bool isMute = false;
+        public float saveVolume = 0;
+        
+        AudioManager _audioManager;
+        
         void Awake()
         {
             source = GetComponent<AudioSource>();
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (isMute)
+                {
+                    UnMute();
+                    isMute = false;
+                }
+                else
+                {
+                    Mute();
+                    isMute = true;
+                }
+            }
+        }
+
+        void SaveVolume()
+        {
+            saveVolume = _audioManager.source.volume;
+        }
+
+        void Mute()
+        {
+            // _source.volume = 0;
+        }
+
+        void UnMute()
+        {
+            // _source.volume = saveVolume;
         }
     }
 }
--- Assets/Coding/GameManager.cs
+++ Assets/Coding/GameManager.cs
@@ -5,45 +5,6 @@ namespace Assets.Coding
 {
     public class GameManager : MonoBehaviour
     {
-        AudioManager _audioManager;
-        public bool isMute = false;
-        public float saveVolume = 0;
         
-        void Awake()
-        {
-            _audioManager = FindObjectOfType<AudioManager>();
-        }
-        
-        void Update()
-        {
-            if (Input.GetMouseButtonDown(1))
-            {
-                if (isMute)
-                {
-                    UnMute();
-                    isMute = false;
-                }
-                else
-                {
-                    Mute();
-                    isMute = true;
-                }
-            }
-        }
-
-        void SaveVolume()
-        {
-            saveVolume = _audioManager.source.volume;
-        }
-
-        void Mute()
-        {
-            // _source.volume = 0;
-        }
-
-        void UnMute()
-        {
-            // _source.volume = saveVolume;
-        }
     }
 }
